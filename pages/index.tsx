8211dbbf50cd049837efd0c5de3b1e5a7022291a
quantized-cube <yukimath@gmail.com>
2023-09-23 09:55:01 +0900

    ページ遷移

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import styles from '../styles/Home.module.css';
 import dayjs from 'dayjs';
 import prism from 'prismjs'
 import { useEffect } from 'react';
+import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
+import Link from 'next/link';
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -35,70 +37,84 @@ export type Post = {
 };
 
 type StaticProps = {
-  post: Post | null;
+  posts: Post[];
 };
 
-export const getStaticProps: GetStaticProps<{}> = async () => {
-  const database = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID || '',
-    filter: {
-      and: [{
-        property: 'Published',
-        checkbox: {
-          equals: true
-        }
+export const getPosts = async (slug?: string) => {
+  let database: QueryDatabaseResponse | undefined = undefined
+  if (slug) {
+    database = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID || '',
+      filter: {
+        and: [{
+          property: 'Slug',
+          rich_text: {
+            equals: slug
+          }
 
-      }]
-    },
-    sorts: [
-      {
-        timestamp: 'created_time',
-        direction: 'descending'
-      }
-    ]
+        }]
+      },
+    })
+  } else {
+    database = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID || '',
+      filter: {
+        and: [{
+          property: 'Published',
+          checkbox: {
+            equals: true
+          }
+
+        }]
+      },
+      sorts: [
+        {
+          timestamp: 'created_time',
+          direction: 'descending'
+        }
+      ]
+    })
+  }
+  if (!database) return []
+  const posts: Post[] = [];
+  database.results.forEach((page, index) => {
+    if (!('properties' in page)) {
+      posts.push({
+        id: page.id,
+        title: null,
+        slug: null,
+        createdTs: null,
+        lastEditedTs: null,
+        contents: []
+      })
+      return
+    }
+    let title: string | null = null
+    if (page.properties['Name'].type === 'title') {
+      title = page.properties['Name'].title[0]?.plain_text ?? null;
+    }
+    let slug: string | null = null;
+    if (page.properties['Slug'].type === 'rich_text') {
+      slug = page.properties['Slug'].rich_text[0]?.plain_text ?? null;
+    }
+    posts.push({
+      id: page.id,
+      title,
+      slug,
+      createdTs: page.created_time,
+      lastEditedTs: page.last_edited_time,
+      contents: []
+    })
   })
-  // console.dir(database, { depth: null })
+  return posts
+}
 
-  // const blocks = await notion.blocks.children.list({
-  //   block_id: database.results[0]?.id,
-  // })
-  // console.dir(blocks, { depth: null })
-
-  const page = database.results[0];
-  if (!page) {
-    return {
-      props: {
-        post: null
-      },
-    }
-  }
-  if (!('properties' in page)) {
-    return {
-      props: {
-        post: {
-          id: page.id,
-          title: null,
-          slug: null,
-          createdTs: null,
-          lastEditedTs: null,
-          contents: []
-        }
-      },
-    }
-  }
-  let title: string | null = null
-  if (page.properties['Name'].type === 'title') {
-    title = page.properties['Name'].title[0]?.plain_text ?? null;
-  }
-  let slug: string | null = null;
-  if (page.properties['Slug'].type === 'rich_text') {
-    slug = page.properties['Slug'].rich_text[0]?.plain_text ?? null;
-  }
-  const blocks = await notion.blocks.children.list({
-    block_id: page.id,
+export const getPostContents = async (post: Post) => {
+  const blockResponse = await notion.blocks.children.list({
+    block_id: post.id,
   })
   const contents: Content[] = [];
-  blocks.results.forEach((block) => {
+  blockResponse.results.forEach((block) => {
     if (!('type' in block)) {
       return
     }
@@ -135,83 +151,91 @@ export const getStaticProps: GetStaticProps<{}> = async () => {
         })
     }
   })
+  return contents
+}
 
-  const post: Post = {
-    id: page.id,
-    title,
-    slug,
-    createdTs: page.created_time,
-    lastEditedTs: page.last_edited_time,
-    contents
-  };
-
-  console.dir(post, { depth: null })
+export const getStaticProps: GetStaticProps<{}> = async () => {
+  const posts = await getPosts();
+  const contentsList = await Promise.all(
+    posts.map((post) => {
+      return getPostContents(post)
+    })
+  )
+  posts.forEach((post, index) => {
+    post.contents = contentsList[index]
+  })
   return {
-    props: { post }
+    props: { posts }
   };
 }
 
-const Home: NextPage<StaticProps> = ({ post }) => {
+const Home: NextPage<StaticProps> = ({ posts }) => {
   // console.log(post)
-  useEffect(() => {
-    prism.highlightAll()
-  }, [])
 
-  if (!post) return null;
+  // useEffect(() => {
+  //   prism.highlightAll()
+  // }, [])
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.post}>
-        <h1 className={styles.title}>{post.title}</h1>
-        <div className={styles.timestampWrapper}>
-          <div>
-            <div className={styles.timestamp}>
-              作成日時：{' '}
-              {
-                dayjs(post.createdTs).format('YYYY-MM-DD HH:mm:ss')
-              }
-            </div>
-            <div className={styles.timestamp}>
-              更新日時：{' '}
-              {
-                dayjs(post.lastEditedTs).format('YYYY-MM-DD HH:mm:ss')
-              }
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <h1 className={styles.title}>
+            <Link href={`/post/${encodeURIComponent(post.slug ?? '')}`}>
+              {post.title}
+            </Link>
+          </h1>
+          <div className={styles.timestampWrapper}>
+            <div>
+              <div className={styles.timestamp}>
+                作成日時：{' '}
+                {
+                  dayjs(post.createdTs).format('YYYY-MM-DD HH:mm:ss')
+                }
+              </div>
+              <div className={styles.timestamp}>
+                更新日時：{' '}
+                {
+                  dayjs(post.lastEditedTs).format('YYYY-MM-DD HH:mm:ss')
+                }
+              </div>
             </div>
           </div>
+          <div>
+            {
+              post.contents.map((content, index) => {
+                const key = `${post.id}_${index}`
+                switch (content.type) {
+                  case 'heading_2':
+                    return (
+                      <h2 key={key} className={styles.heading2}>{content.text}</h2>
+                    );
+                  case 'heading_3':
+                    return (
+                      <h3 key={key} className={styles.heading3}>{content.text}</h3>
+                    );
+                  case 'paragraph':
+                    return (
+                      <p key={key} className={styles.paragraph}>{content.text}</p>
+                    );
+                  case 'code':
+                    return (
+                      <pre className={`${styles.code} lang-${content.language}`}>
+                        <code>{content.text}</code>
+                      </pre>
+                    );
+                  case 'quote':
+                    return (
+                      <blockquote key={key} className={styles.quote}>
+                        {content.text}
+                      </blockquote>
+                    );
+                }
+              })
+            }
+          </div>
         </div>
-        <div>
-          {
-            post.contents.map((content, index) => {
-              const key = `${post.id}_${index}`
-              switch (content.type) {
-                case 'heading_2':
-                  return (
-                    <h2 key={key} className={styles.heading2}>{content.text}</h2>
-                  );
-                case 'heading_3':
-                  return (
-                    <h3 key={key} className={styles.heading3}>{content.text}</h3>
-                  );
-                case 'paragraph':
-                  return (
-                    <p key={key} className={styles.paragraph}>{content.text}</p>
-                  );
-                case 'code':
-                  return (
-                    <pre className={`${styles.code} lang-${content.language}`}>
-                      <code>{content.text}</code>
-                    </pre>
-                  );
-                case 'quote':
-                  return (
-                    <blockquote key={key} className={styles.quote}>
-                      {content.text}
-                    </blockquote>
-                  );
-              }
-            })
-          }
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
